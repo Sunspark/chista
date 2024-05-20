76ca9c4b7d0d99667031e9170bee0d57446fd07b
Will List <williamealist@gmail.com>
2024-05-20 10:55:46 +0100

    Related Logical Entities

--- database_declarations/LogicalEntity.sql
+++ database_declarations/LogicalEntity.sql
@@ -37,6 +37,7 @@ CREATE TABLE rv_s_LogicalEntity
   
   , LogicalEntityName varchar(100) NULL
   , isDeleted boolean NULL DEFAULT 0
+  , "Description" varchar(1000) NULL
 )
 ;
 CREATE UNIQUE INDEX "PK_rv_s_LogicalEntity" ON "rv_s_LogicalEntity" (
@@ -60,3 +61,36 @@ CREATE UNIQUE INDEX "UK_rv_l_ModelLogicalEntity" ON "rv_l_ModelLogicalEntity" (
   , LogicalEntityHashKey
 );
 
+CREATE TABLE rv_l_LogicalEntityLogicalEntity_Related
+(
+    LogicalEntityLogicalEntity_RelatedHashKey char(32) NOT NULL
+  , LoadDate datetime NOT NULL
+  , RecordSource nvarchar(500) NOT NULL
+  
+  , PrimaryLogicalEntityHashKey varchar(100) NOT NULL
+  , SecondaryLogicalEntityHashKey varchar(100) NOT NULL
+)
+;
+CREATE UNIQUE INDEX "PK_rv_l_LogicalEntityLogicalEntity_Related" ON "rv_l_LogicalEntityLogicalEntity_Related" (LogicalEntityLogicalEntity_RelatedHashKey);
+CREATE UNIQUE INDEX "UK_rv_l_LogicalEntityLogicalEntity_Related" ON "rv_l_LogicalEntityLogicalEntity_Related" (
+  PrimaryLogicalEntityHashKey
+  , SecondaryLogicalEntityHashKey
+);
+
+CREATE TABLE rv_s_LogicalEntityLogicalEntity_Related
+(
+    LogicalEntityLogicalEntity_RelatedHashKey char(32) NOT NULL
+  , LoadDate datetime NOT NULL
+  , RecordSource nvarchar(500) NOT NULL
+  , HashDiff char(32) NOT NULL
+  
+  , ForwardWords varchar(100) NULL
+  , ReverseWords varchar(100) NULL
+  , EffectiveStart datetime NULL
+  , EffectiveEnd datetime NULL
+)
+;
+CREATE UNIQUE INDEX "PK_rv_s_LogicalEntityLogicalEntity_Related" ON "rv_s_LogicalEntityLogicalEntity_Related" (
+  LogicalEntityLogicalEntity_RelatedHashKey
+  , LoadDate
+);
