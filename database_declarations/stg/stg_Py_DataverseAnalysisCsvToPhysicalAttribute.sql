ed3d445f801ad581a8f33fc198e084968eb79595
Will List <williamealist@gmail.com>
2024-07-22 13:48:59 +0100

    Import dataverse analysis CSV to physical attribute

---
+++ database_declarations/stg/stg_Py_DataverseAnalysisCsvToPhysicalAttribute.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "stg_Py_DataverseAnalysisCsvToPhysicalAttribute" (
+"PhysicalAttributeHashKey" TEXT,
+  "PhysicalAttributeKeyPhrase" TEXT,
+  "SERVER_NAME" TEXT,
+  "entity_logical_name" TEXT,
+  "Logical Name" TEXT,
+  "Schema Name" TEXT,
+  "Display Name" TEXT,
+  "Attribute Type" TEXT,
+  "Custom Attribute" INTEGER,
+  "Type" TEXT,
+  "Additional data" TEXT,
+  "Description" TEXT,
+  "Tags" TEXT,
+  "RecordSource" TEXT,
+  "LoadDate" TIMESTAMP,
+  "HashDiff" TEXT
+);
