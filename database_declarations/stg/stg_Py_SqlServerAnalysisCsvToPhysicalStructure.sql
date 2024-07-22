1b450fb9f959ba811ca2dfe1515d41fad4ab1c57
Will List <williamealist@gmail.com>
2024-07-22 16:39:48 +0100

    Import SQLServer Analysis CSV to Physical Structure

---
+++ database_declarations/stg/stg_Py_SqlServerAnalysisCsvToPhysicalStructure.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "stg_Py_SqlServerAnalysisCsvToPhysicalStructure" (
+"PhysicalStructureHashKey" TEXT,
+  "PhysicalStructureKeyPhrase" TEXT,
+  "SERVER_NAME" TEXT,
+  "DATABASE_NAME" TEXT,
+  "SCHEMA_NAME" TEXT,
+  "TABLE_NAME" TEXT,
+  "ROW_COUNT" INTEGER,
+  "TABLE_TYPE" TEXT,
+  "TABLE_TYPE_DESCRIPTION" TEXT,
+  "COLUMN_COUNT" INTEGER,
+  "TABLE_DESCRIPTION" TEXT,
+  "Tags" TEXT,
+  "RecordSource" TEXT,
+  "LoadDate" TIMESTAMP,
+  "HashDiff" TEXT
+)
--- database_declarations/stg/stg_Py_SqlServerAnalysisCsvToPhysicalStructureTag.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "stg_Py_SqlServerAnalysisCsvToPhysicalStructureTag" (
-  "IS_REFERENCE" TEXT,
-  "RecordSource" TEXT,
-  "LoadDate" TIMESTAMP,
-  "PhysicalStructureHashKey" TEXT
-)
