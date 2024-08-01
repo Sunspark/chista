c821e46307f6cc4cd63ec4e1dabc93a52ff7a952
Will List <williamealist@gmail.com>
2024-08-01 17:19:05 +0100

    Refactoring outputs for schema compare

--- database_declarations/out/out_PhysicalAttributeCurrentForSchemaCompare.sql
+++ database_declarations/out/out_PhysicalAttributeCurrentForSchemaCompare.sql
@@ -1,5 +1,6 @@
 CREATE VIEW out_PhysicalAttributeCurrentForSchemaCompare AS
 
+-- Structures from SQL Server
 SELECT
     SERVER_NAME
   , "DATABASE_NAME"
@@ -16,7 +17,8 @@ FROM
 	out_SQLServerPhysicalAttributeOutputForAnalysis
 
 UNION
-	
+
+-- Structures from Dataverse via XRM Toolbox
 SELECT
     "SERVER_NAME"
   , "DATABASE_NAME"
@@ -31,4 +33,31 @@ SELECT
 	, NULL AS NUMERIC_SCALE
 FROM
 	out_DataversePhysicalAttributeOutputForAnalysis
+
+UNION
+
+-- Structures from Dataverse via Synapse Link JSON
+SELECT
+    "SERVER_NAME"
+  , "DATABASE_NAME"
+  , "SCHEMA_NAME"
+  , "entity_name" AS TABLE_NAME
+  , "name" AS COLUMN_NAME
+
+  , NULL AS IS_NULLABLE
+  , "dataType" AS DATA_TYPE
+  , "maxLength" AS CHARACTER_MAXIMUM_LENGTH
+  , "precision" AS NUMERIC_PRECISION
+  , "scale" AS NUMERIC_SCALE
+ 
+FROM
+	rv_h_PhysicalAttribute ps
+	INNER JOIN rv_s_PhysicalAttribute_SynLinkJSON AS sat ON (
+    ps.PhysicalAttributeHashKey = sat.PhysicalAttributeHashKey
+    AND sat.LoadDate = (
+      SELECT MAX(z.LoadDate)
+      FROM rv_s_PhysicalAttribute_SynLinkJSON AS z
+      WHERE z.PhysicalAttributeHashKey = sat.PhysicalAttributeHashKey
+    )
+  )
 ;
