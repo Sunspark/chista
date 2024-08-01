de87c80e488acea4e35813df015cc15468f91da8
Will List <williamealist@gmail.com>
2024-08-01 17:08:55 +0100

    Refactoring schema compare view Hiding debug in script

--- database_declarations/out/out_PhysicalStructureCurrentForSchemaCompare.sql
+++ database_declarations/out/out_PhysicalStructureCurrentForSchemaCompare.sql
@@ -1,5 +1,6 @@
 CREATE VIEW out_PhysicalStructureCurrentForSchemaCompare AS
 
+-- Structures from SQL Server
 SELECT
     SERVER_NAME
   , "DATABASE_NAME"
@@ -10,7 +11,8 @@ FROM
 	out_SQLServerPhysicalStructureOutputForAnalysis
 
 UNION
-	
+
+-- Structures from Dataverse via XRM Toolbox
 SELECT
     "SERVER_NAME"
   , "DATABASE_NAME"
@@ -19,4 +21,25 @@ SELECT
   , "CountAttributes" AS COLUMN_COUNT
 FROM
 	out_DataversePhysicalStructureOutputForAnalysis
+
+UNION
+
+-- Structures from Dataverse via Synapse Link JSON
+SELECT
+    "SERVER_NAME"
+  , "DATABASE_NAME"
+  , "SCHEMA_NAME"
+  , "name" AS TABLE_NAME
+  , NULL AS COLUMN_COUNT
+
+FROM
+	rv_h_PhysicalStructure ps
+	INNER JOIN rv_s_PhysicalStructure_SynLinkJSON AS sat ON (
+    ps.PhysicalStructureHashKey = sat.PhysicalStructureHashKey
+    AND sat.LoadDate = (
+      SELECT MAX(z.LoadDate)
+      FROM rv_s_PhysicalStructure_SynLinkJSON AS z
+      WHERE z.PhysicalStructureHashKey = sat.PhysicalStructureHashKey
+    )
+  )
 ;
