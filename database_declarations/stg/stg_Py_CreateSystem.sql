9d65604a855f4ab4fd2b8e795260fd46f83d0e99
Will List <williamealist@gmail.com>
2024-08-07 16:31:55 +0100

    Importing Systems from CSV

--- database_declarations/stg/stg_Py_CreateSystem.sql
+++ database_declarations/stg/stg_Py_CreateSystem.sql
@@ -3,6 +3,7 @@ CREATE TABLE stg_Py_CreateSystem
     SystemKeyPhrase varchar(100) NOT NULL
   , SystemName varchar(100) NULL
   , "Description" varchar(1000) NULL
+  , isDeleted int NULL
   
   , LoadDate datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
   , RecordSource nvarchar(500) NULL
